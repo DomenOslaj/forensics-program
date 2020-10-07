49b074a791b6d764a6fec2f653c0634cb5d07a81
Domen Oslaj <doslaj@gmail.com>
2020-10-07 22:16:50 +0200

    added dict content

--- program.py
+++ program.py
@@ -4,13 +4,63 @@ with open("dna.txt", "r") as dna_file:
 
 # dict inside of dict
 characteristics = {
-
+    "hair_color": {
+        "black": "CCAGCAATCGC",
+        "brown": "GCCAGTGCCG",
+        "blonde": "TTAGCTATCGC"
+    },
+    "facial_shape": {
+        "square": "GCCACGG",
+        "round": "ACCACAA",
+        "oval": "AGGCCTCA"
+    },
+    "eye_color": {
+        "blue": "TTGTGGTGGC",
+        "green": "GGGAGGTGGC",
+        "brown": "AAGTAGTGAC"
+    },
+    "gender": {
+        "female": "TGAAGGACCTTC",
+        "male": "TGCAGGAACTTC"
+    },
+    "race": {
+        "white": "AAAACCTCA",
+        "black": "CGACTACAG",
+        "asian": "CGCGGGCCG"
+    }
 }
 
 suspects = {
+    "Eva": {
+        "gender": "female",
+        "race": "white",
+        "hair_color": "blonde",
+        "eye_color": "blue",
+        "face_shape": "oval"
+    },
+    "Larisa": {
+        "gender": "female",
+        "race": "white",
+        "hair_color": "brown",
+        "eye_color": "brown",
+        "face_shape": "oval"
+    },
+    "Matej": {
+        "gender": "male",
+        "race": "white",
+        "hair_color": "black",
+        "eye_color": "blue",
+        "face_shape": "oval"
+    },
+    "Miha": {
+        "gender": "male",
+        "race": "white",
+        "hair_color": "brown",
+        "eye_color": "green",
+        "face_shape": "square"
+    }
 
 }
-
 
 
 
